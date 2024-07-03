47034c001be82d05493912aebb1a8d3fc94f30f7
GoldenMenina <menezessuraia@gmail.com>
2024-07-03 15:55:09 +0100

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -58,20 +58,20 @@ export default function App({
         </Head>
         <Component {...pageProps} />
         
-  <script src="/assets/js/jquery.js"></script>
-    <script src="/assets/js/popper.min.js"></script>
-    <script src="/assets/js/bootstrap.min.js"></script>
-    <script src="/assets/js/owl.js"></script>
-    <script src="/assets/js/wow.js"></script>
-    <script src="/assets/js/validation.js"></script>
-    <script src="/assets/js/jquery.fancybox.js"></script>
-    <script src="/assets/js/appear.js"></script>
-    <script src="/assets/js/scrollbar.js"></script>
-    <script src="/assets/js/nav-tool.js"></script>
+  <script defer src="/assets/js/jquery.js"></script>
+    <script defer src="/assets/js/popper.min.js"></script>
+    <script defer src="/assets/js/bootstrap.min.js"></script>
+    <script defer src="/assets/js/owl.js"></script>
+    <script defer src="/assets/js/wow.js"></script>
+    <script defer src="/assets/js/validation.js"></script>
+    <script defer src="/assets/js/jquery.fancybox.js"></script>
+    <script defer src="/assets/js/appear.js"></script>
+      <script defer src="/assets/js/scrollbar.js"></script>
+    <script defer src="/assets/js/nav-tool.js"></script>
 
     
-    <script src="/assets/js/script.js"></script>
-    <script src="/assets/js/mainscript.js"></script>
+    <script defer src="/assets/js/script.js"></script>
+    <script defer src="/assets/js/mainscript.js"></script>
       </Layout>
   );
 }
