4bd6532680cdff94e07f2ed0e8acd9788c3a4206
GoldenMenina <menezessuraia@gmail.com>
2024-07-13 12:28:55 +0100

    update dashboard.js

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 
 
 const supabase = createClient(
-NEXT_PUBLIC_SUPABASE_URL,
-NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default function Admin() {
