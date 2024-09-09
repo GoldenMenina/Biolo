0c6d72fa2e8bd90df17d43b813a4bb8c3be8c40f
GoldenMenina <menezessuraia@gmail.com>
2024-09-09 12:21:59 +0000

    jlkj2

--- .next/static/webpack/webpack.ee00f7f9ee8585a0.hot-update.js
+++ .next/static/webpack/webpack.ee00f7f9ee8585a0.hot-update.js
@@ -11,21 +11,8 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ee00f7f9ee8585a0"; }
+/******/ 	__webpack_require__.h = function() { return "ee5ee21c0b08ed4c"; }
 /******/ }();
-/******/ 
-/******/ /* webpack/runtime/compat */
-/******/ 
-/******/ 
-/******/ // noop fns to prevent runtime errors during initialization
-/******/ if (typeof self !== "undefined") {
-/******/ 	self.$RefreshReg$ = function () {};
-/******/ 	self.$RefreshSig$ = function () {
-/******/ 		return function (type) {
-/******/ 			return type;
-/******/ 		};
-/******/ 	};
-/******/ }
 /******/ 
 /******/ }
 );
